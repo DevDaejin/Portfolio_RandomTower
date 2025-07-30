a2329b1f0a4abf2db84445a44f69730637dfebbb
Daejin <whry321@naver.com>
2025-07-30 23:37:41 +0900

    누락 데이터

---
+++ RandomTower_Unity/Assets/Scripts/Global/DTO/OptionSaveData.cs
@@ -0,0 +1,11 @@
+using System;
+
+[Serializable]
+public class OptionSaveData
+{
+    public int LanguageCode = 0;
+    public int ResolutionCode = 0;
+    public int ScreenModeCode = 0;
+    public float BGM = 1;
+    public float SFX = 1;
+}
